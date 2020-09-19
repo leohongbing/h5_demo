b26eae4f7625ef1848e9114dcb0c36fbdaa0514f
雷洪兵 <leihb@dajiashequ.com>
2020-09-19 14:06:11 +0800

    ts 函数

---
+++ TypeScript/函数.ts
@@ -0,0 +1,78 @@
+// 具名函数
+function f1(n1: number, n2:number): number {
+    return n1 + n2
+}
+
+// 匿名函数
+const f2: (n1: number, n2: number) => number =  function(x: number, y: number): number {
+    return x + y;
+}
+
+// 推断类型
+const f3: (n1: number, n2: number) => number =  function(x, y) {
+    return x + y;
+}
+
+// 可选参数 默认参数 剩余参数
+function f4(name1: string, name2?: string, name3='leo', ...restArg: string[]) {
+    console.log(name1, name2, name3, restArg)
+}
+
+// f4('bob')
+f4('n1', 'n2', undefined, 'a', 'b')
+
+// this
+interface Card {
+    suit: string;
+    card: number;
+}
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
+let deck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    // NOTE: The function now explicitly specifies that its callee must be of type Deck
+    createCardPicker: function(this: Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+
+// 方法重载
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x: {suit: string; card: number; }[]): number;
+function pickCard(x: number): {suit: string; card: number; };
+function pickCard(x): any {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
