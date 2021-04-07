673d4a4aee1639fa8149415e4df9e2c41f2ef740
雷洪兵 <lhb@yzgong.com>
2021-04-07 11:05:32 +0800

    内存泄漏

--- vue/build/webpack.base.conf.js
+++ vue/build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/handComponent.js'
+    app: './src/router.js'
   },
   output: {
     path: config.build.assetsRoot,
